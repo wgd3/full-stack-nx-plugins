cc28c2dc2edce3b4f2b1a1e4bc9b0a936552abe5
wgd3 <wallace.daniel3@gmail.com>
2023-04-05 16:42:06 -0400

    changing docs baseUrl

--- packages/nx-sass-lib/docs/docusaurus.config.js
+++ packages/nx-sass-lib/docs/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'nx-sass-lib',
   tagline: 'Dinosaurs are cool',
-  url: 'https://wgd3.github.io/full-stack-nx-plugins/',
-  baseUrl: '/',
+  url: 'https://wgd3.github.io',
+  baseUrl: '/full-stack-nx-plugins',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
