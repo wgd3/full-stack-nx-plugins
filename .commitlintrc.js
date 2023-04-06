29b261820b7a2348590c022dfd511e6ad5034bcd
wgd3 <wallace.daniel3@gmail.com>
2023-04-06 14:22:55 -0400

    chore: working on commit/release config again

---
+++ .commitlintrc.js
@@ -0,0 +1,37 @@
+
+const {
+  utils: { getProjects },
+} = require('@commitlint/config-nx-scopes');
+
+
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [
+        ...(await getProjects(
+          ctx,
+          ({ name, projectType }) => !name.includes('e2e')
+        )),
+        'repo',
+        'docs',
+      ],
+    ],
+    'type-enum': () => [2, 'always', [
+      'build',
+      'ci',
+      'docs',
+      'feat',
+      'fix',
+      'perf',
+      'refactor',
+      'revert',
+      'style',
+      'test',
+      'chore'
+    ]]
+  },
+};
+
---
+++ packages/nx-sass-lib/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+# 1.0.0 (2023-04-06)
+
+
+
+# 1.0.0 (2023-04-06)
+
+
+
+# 1.0.0 (2023-04-06)
