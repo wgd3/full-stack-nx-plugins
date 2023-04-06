1664ebfa6a09ee6a554ad82913f8028fe5e6d83f
wgd3 <wallace.daniel3@gmail.com>
2023-04-06 16:27:59 -0400

    changed broken doc link behavior

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://wgd3.github.io',
   baseUrl: '/full-stack-nx-plugins/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'wgd3', // Usually your GitHub org/user name.
